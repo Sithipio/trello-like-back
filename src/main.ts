a7fd0fb0251af1c9e94f257d037475d4c8a7bcfb
Berezos Valentyn <valentyn.berezos@inventorsoft.co>
2022-08-18 20:08:47 +0300

    Deploy progect

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
      next();
    });*/
 
-  const port = 3000;
+  const port = process.env.PORT;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
