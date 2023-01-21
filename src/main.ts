0083b26927829bee4223286cfd6985be7633dec8
Valentyn Berezos <valentyn.berezos@oril.co>
2023-01-21 15:57:40 +0200

    Test deploy (v3)

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger();
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-
-  app.enableCors();
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
